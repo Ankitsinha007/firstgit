08b69dabcc2aab7fdc6aaa941d229056824da9ef
Ankit Kumar <97171937+Ankitsinha007@users.noreply.github.com>
2023-07-01 12:34:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("Hello Github Community")
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all[10])
+// console.log(document.links)
+// console.log(document.forms)
+
+// var header = document.getElementById("header-title");
+// console.log(header);
+
+// // header.textContent = "Hello";
+// // header.innerText = "GoodBye";
+
+// header.innerHTML = '<h3>Hello</h3>'
+
+// var itemTitle = document.getElementsByClassName("title-items");
+// console.log(itemTitle);
+
+ // header.style.borderBottom = "solid 5px Blue";
+
+ var item = document.getElementsByClassName("list-group-item");
+ console.log(item);
+ 
+ item.textContent = "Hello 2";
+ item.style.color = "Blue";
+
+ 
