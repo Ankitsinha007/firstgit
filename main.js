ae0a56d375c44031caec04534c9471f9404cf726
Ankit Kumar <97171937+Ankitsinha007@users.noreply.github.com>
2023-07-02 18:54:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,125 +1,67 @@
-console.log(document.head)
-console.log(document.body)
-console.log(document.all[10])
-console.log(document.links)
-console.log(document.forms)
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-var header = document.getElementById("header-title");
-console.log(header);
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
-// header.textContent = "Hello";
-// header.innerText = "GoodBye";
+// Add item
+function addItem(e){
+  e.preventDefault();
 
-header.innerHTML = '<h3>Hello</h3>'
+  // Get input value
+  var newItem = document.getElementById('item').value;
 
-var itemTitle = document.getElementsByClassName("title-items");
-console.log(itemTitle);
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
 
- header.style.borderBottom = "solid 5px Blue";
+  // Create del button element
+  var deleteBtn = document.createElement('button');
 
- var item = document.getElementsByClassName("list-group-item");
- console.log(item);
- 
- item.textContent = "Hello 2";
- item.style.color = "Blue";
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
 
-// querrySelectors
+  // Append button to li
+  li.appendChild(deleteBtn);
 
-var header = document.querySelector("main-header");
-header.style.borderBottom = "solid 4px #ccc";
+  // Append li to list
+  itemList.appendChild(li);
+}
 
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
 
-var input = document.querySelector("input");
-input.value = "Hello World";
-
-var submit = document.querySelector('input[type = "submit"]');
-submit.value = "SEND";
- 
-
-var item = document.querySelector('.list-group-item');
-item.style.color = "red";
-
-// TRAVERSING THE DOM
-
-var itemList = document.querySelector("#items");
-
-
-// // parentNode
-console.log(itemList.parentNode);
-
-itemList.parentNode.style.backgroundColor = "blue";
-
-console.log(itemList.parentNode.parentNode);
-
-
-// parentelememt
-console.log(itemList.parentElement);
-
-itemList.parentNode.style.backgroundColor = "blue";
-
-console.log(itemList.parentElement.parentElement);
-
-
-//childNodes
-console.log(itemList.childNodes);
-
-console.log(itemList.children);
-console.log(itemList.children[1]);
-
-// itemList.children[1].style.backgroungcolor = 'yellow';
-
-console.log(itemList.firstChild);
-
-//firstElementchild
-
-console.log(itemList.firstElementChild);
-itemList.firstElementChild = "Hello 1";
-
-//lastElementchild
-console.log(itemList.lastElementChild);
-itemList.lastElementChild = "Hello 4";
-
-//nextsibling
-console.log (itemList.nextSibling);
-
-//nextElementsibling
-console.log(itemList.nextElementSibling);
-
-//previousSibling
-console.log(itemList.previousSibling);
-
-//previousElementsibling
-console.log(itemList.previousElementSibling);
-itemList.previousElementSibling.style.color = "green";
-
-//createElement
-
-//create a div
-var newDiv=document.createElement("div");
-
-//Add class
-newDiv.className = "hello";
-
-//Add id
-newDiv.id = "hello 1";
-
-// Add attribute
-newDiv.setAttribute = ('title', 'hello div');
-
-//create text node
-var newDivText = document.createTextNode("Hello world");
-
-// Add text to div
-newDiv.appendChild(newDivText);
-
-var container = document.querySelector('header, container');
-var h1 = document.querySelector('header h1');
-
-console.log(newDiv);
-
-newDiv.style.fontSize = "30px";
-
-container.insertBefore(newDiv , h1);
-
-
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
